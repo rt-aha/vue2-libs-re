0506a967acb44df396600275ca3576f62424e5f4
tsai_ray <silentice1534@gmail.com>
2020-03-08 16:39:20 +0800

    update

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    vue js scss
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue js scss',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/index';
+</style>
