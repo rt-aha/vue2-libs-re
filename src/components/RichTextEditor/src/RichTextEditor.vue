00339dac6a86b1762bade50e86bcf785bb194cea
ray_tsai <ray.tsai@silkrode.com.tw>
2021-04-12 17:01:13 +0800

    [dev] rich editor

--- src/components/RichTextEditor/src/RichTextEditor.vue
+++ src/components/RichTextEditor/src/RichTextEditor.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="re-">
-    RichTextEditor
+  <div class="re-rich-text-editor">
+    <div class="quill-editor" ref="quillEditor"></div>
   </div>
 </template>
 
 <script>
+import Quill from 'quill';
+
+// const editor = new Quill('#editor', options);
+
 export default {
   name: 'ReRichTextEditor',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data() {
+    return {
+      // quillContainer: '.quill-editor',
+      quillEditor: null,
+      options: {
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, 3, 4, false] }],
+            ['bold', 'italic', 'underline'],
+          ],
+        },
+        theme: 'snow',
+        formats: ['bold', 'underline', 'header', 'italic'],
+      },
+    };
+  },
+  methods: {
+    init() {
+      this.quillEditor = new Quill(this.$refs.quillEditor, this.options);
+
+      this.quillEditor.root.innerHTML = this.value;
+
+      this.quillEditor.on('text-change', () => this.update());
+    },
+  },
+  mounted() {
+    this.init();
+  },
 };
 </script>
 
--- src/views/comps/RichTextEditorPage.vue
+++ src/views/comps/RichTextEditorPage.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-   <re-rich-text-editor />
+  <div class="view-rich-text-editor">
+
+      <re-rich-text-editor v-model="editorValue"/>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'RichTextEditorPage',
+  data() {
+    return {
+      editorValue: 'allalls',
+    };
+  },
 };
 </script>
+
+<style lang="scss">
+.view-rich-text-editor {
+  @include box-padding(10px)
+}
+</style>
