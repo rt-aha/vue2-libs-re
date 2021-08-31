6683462f00d7e154c5ee35f38423b428acaad3d0
tsai_ray <silentice1534@gmail.com>
2021-08-31 22:22:34 +0800

    feat: dialog component

--- src/views/others/Dialog.vue
+++ src/views/others/Dialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-dialog">
     <re-button @click="openDialog('prop')">開啟彈窗(prop)</re-button>
-    <!-- <re-button @click="openDialog('slot')">開啟彈窗(slot)</re-button> -->
+    <re-button @click="openDialog('slot')">開啟彈窗(slot)</re-button>
 
     <re-dialog
       :visible.sync="dialogVisible.prop"
@@ -12,7 +12,7 @@
       :data="{ a: 1, b: 2 }"
     />
 
-    <!-- <re-dialog :visible.sync="dialogVisible.slot" footerPosition="center">
+    <re-dialog :visible.sync="dialogVisible.slot" footerPosition="center">
       <template #header>
         <re-title type="dialog" :mt="false">彈窗標題 slot</re-title>
       </template>
@@ -20,7 +20,7 @@
       <template #footer>
         <re-button @click="closeDialog('slot')">取消</re-button>
       </template>
-    </re-dialog> -->
+    </re-dialog>
   </div>
 </template>
 
