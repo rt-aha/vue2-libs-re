8972729bf1cd9f405ff2b8df195d8c605d80a53d
tsai_ray <silentice1534@gmail.com>
2021-09-01 22:49:31 +0800

    feat: re-badge component

--- src/components/Badge/src/Badge.vue
+++ src/components/Badge/src/Badge.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="re-badge">
-    <slot></slot>
-    <div class="badge-box">
-      <div class="badge-box__value-cell">
+  <div class="re-badge" :class="{ 're-badge--position': position !== '' }">
+    <div class="re-badge__content" :class="{ 're-badge__content--position--left': position === 'left' }">
+      <slot></slot>
+    </div>
+    <div
+      class="badge-box"
+      :class="[`badge-box--color-type--${colorType}`, `badge-box--position--${position}`, { 'badge-box--dot': dot }]"
+    >
+      <div class="badge-box__value-cell" :class="{ 'badge-box__value-cell--dot': dot }">
         <span class="badge-box__value-cell__value">{{ value }}</span>
       </div>
     </div>
@@ -10,7 +15,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'ReBadge',
   props: {
@@ -18,38 +22,111 @@ export default {
       type: [String, Number],
       default: '12',
     },
+    colorType: {
+      type: String,
+      default: 'default',
+      validator(val) {
+        return ['default', 'brown', 'green', 'red'].indexOf(val) > -1;
+      },
+    },
+    position: {
+      type: String,
+      default: '',
+      validator(val) {
+        return ['', 'left', 'right'].indexOf(val) > -1;
+      },
+    },
+    dot: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 .re-badge {
-  position: relative;
   display: inline-block;
-}
+  position: relative;
 
-.badge-box {
-  height: 20px;
-  min-width: 20px;
-  position: absolute;
-  top: -10px;
-  right: -10px;
-  background-color: $c-badge;
-  border-radius: 10px;
-
-  &__value-cell {
-    display: flex;
-    justify-content: center;
+  // position === left or position === right
+  &--position {
+    display: inline-flex;
     align-items: center;
-    height: 100%;
-    @include box-padding(5px);
+  }
 
-    &__value {
-      display: inline-block;
-      @include font-style($c-white, 12px);
+  &__content {
+    &--position {
+      &--left {
+        order: 2;
+      }
     }
   }
 }
 
+.badge-box {
+  @include position(tr, -10px, 0);
+  transform: translateX(50%);
+  min-width: 20px;
+  height: 20px;
+  border-radius: 10px;
+
+  &--dot {
+    @include circle(8px);
+    top: 0;
+    transform: translate(50%, -50%);
+    min-width: 8px;
+  }
+
+  &--color-type {
+    &--default {
+      background-color: $c-badge--default;
+    }
+
+    &--brown {
+      background-color: $c-badge--brown;
+    }
+
+    &--green {
+      background-color: $c-badge--green;
+    }
+
+    &--red {
+      background-color: $c-badge--red;
+    }
+  }
+
+  &--position {
+    &--right {
+      position: static;
+      transform: translateX(0);
+      width: auto;
+      margin-left: 10px;
+    }
+
+    &--left {
+      order: 1;
+      position: static;
+      transform: translateX(0);
+      margin-right: 10px;
+    }
+  }
+
+  &__value-cell {
+    @include box-padding(5px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    &--dot {
+      display: none;
+    }
+
+    &__value {
+      @include font-style($c-white, 12px);
+      display: inline-block;
+    }
+  }
+}
 </style>
--- src/views/data/Badge.vue
+++ src/views/data/Badge.vue
@@ -1,13 +1,71 @@
 <template>
   <div class="page-badge">
-    <re-badge>
-      <re-button>按鈕</re-button>
-    </re-badge>
+    <re-dev-sheet>
+      <re-dev-desc>基本使用</re-dev-desc>
+      <re-badge>
+        <re-button>按鈕</re-button>
+      </re-badge>
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>棕色</re-dev-desc>
+      <re-badge colorType="brown" value="35">
+        <re-button>按鈕</re-button>
+      </re-badge>
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>綠色</re-dev-desc>
+      <re-badge colorType="green">
+        <re-button>按鈕</re-button>
+      </re-badge>
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>紅色，較長的內容</re-dev-desc>
+      <re-badge colorType="red" v-model="hundredTest">
+        <re-button>按鈕</re-button>
+      </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>小紅點，按鈕</re-dev-desc>
+      <re-badge colorType="red" dot>
+        <re-button>按鈕</re-button>
+      </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>小紅點，文字</re-dev-desc>
+      <re-badge colorType="red" dot> 通知 </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>左邊</re-dev-desc>
+      <re-badge colorType="brown" position="left"> 通知 </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>右邊</re-dev-desc>
+      <re-badge colorType="red" position="right"> 通知 </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>左邊，小紅點</re-dev-desc>
+      <re-badge colorType="red" position="right" dot> 通知 </re-badge>
+    </re-dev-sheet>
+
+    <re-dev-sheet>
+      <re-dev-desc>右邊，小綠點</re-dev-desc>
+      <re-badge colorType="green" position="left" dot> 通知 </re-badge>
+    </re-dev-sheet>
   </div>
 </template>
 
 <script>
 export default {
   name: 'PageBadge',
+  data() {
+    return {
+      hundredTest: '100+',
+    };
+  },
 };
 </script>
