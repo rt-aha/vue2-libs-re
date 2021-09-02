24eee8fdec80714e4e4e82f6650586167c0fed68
tsai_ray <silentice1534@gmail.com>
2021-09-02 23:38:34 +0800

    feat: title component

---
+++ src/views/others/Title.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-title">
+    <re-dev-sheet>
+      <re-dev-desc>說明</re-dev-desc>
+      新增一個 type 在組件中，並設定相同名字的 class 在組件中即可使用
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>基本使用</re-dev-desc>
+      <re-title type="app">系統標題</re-title>
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>基本使用</re-dev-desc>
+      <re-title type="demo">demo 使用標題</re-title>
+    </re-dev-sheet>
+    <re-dev-sheet>
+      <re-dev-desc>基本使用</re-dev-desc>
+      <re-title type="dialog">彈窗標題</re-title>
+    </re-dev-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+};
+</script>
+
+<style lang="scss" scoped></style>
